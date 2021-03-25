bedc1294ecc0f7be33752539a02819f6cf745787
Ava Wulkan <ava.wulkan@gmail.com>
2021-03-25 20:56:18 +0100

    Punkt 1: Skapat doubles av de två interfacen och gjort ett första test av metoden payEmployees

--- src/test/java/com/example/EmployeeManagerTest.java
+++ src/test/java/com/example/EmployeeManagerTest.java
@@ -1,5 +1,17 @@
 package com.example;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class EmployeeManagerTest {
+
+    @Test
+    void payEmployeesTest(){
+        EmployeeManager testObject = new EmployeeManager(new EmployeeRepositoryDouble(), new BankServiceDouble());
+
+        int expected = 1;
+        int actual = testObject.payEmployees();
+        Assertions.assertEquals(expected,actual);
+    }
 
 }
