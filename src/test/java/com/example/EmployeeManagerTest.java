d0b8d2c48e39f360fc4881c71c122b54775219cb
Ava Wulkan <ava.wulkan@gmail.com>
2021-03-25 22:20:53 +0100

    Punkt 3: Skapat tester för all kod i payEmployees medoden.

--- src/test/java/com/example/EmployeeManagerTest.java
+++ src/test/java/com/example/EmployeeManagerTest.java
@@ -9,9 +9,11 @@ import java.util.List;
 import static org.mockito.Mockito.*;
 
 class EmployeeManagerTest {
+    String ID = "001";
+    double SALARY = 23000;
 
     @Test
-    void payEmployeesTest(){
+    void payEmployeesDoublesTest(){
         EmployeeManager testObject = new EmployeeManager(new EmployeeRepositoryDouble(), new BankServiceDouble());
 
         int expected = 1;
@@ -23,13 +25,48 @@ class EmployeeManagerTest {
     void payEmployeesMockitoTest(){
         EmployeeRepository mockRepo = mock(EmployeeRepository.class);
         EmployeeManager testObject = new EmployeeManager(mockRepo,mock(BankService.class));
+
         ArrayList<Employee> employees = new ArrayList<>();
-        employees.add(new Employee("001", 23000));
+        employees.add(new Employee(ID, SALARY));
         when(mockRepo.findAll()).thenReturn(employees);
 
         int expected = 1;
+        boolean expectedPaid = true;
         int actual = testObject.payEmployees();
         Assertions.assertEquals(expected,actual);
+        Assertions.assertEquals(expectedPaid, employees.get(0).isPaid());
+    }
+
+    @Test
+    void payEmployeesMockitoNoEmployeesTest(){
+        EmployeeRepository mockRepo = mock(EmployeeRepository.class);
+        EmployeeManager testObject = new EmployeeManager(mockRepo,mock(BankService.class));
+
+        ArrayList<Employee> employees = new ArrayList<>();
+        when(mockRepo.findAll()).thenReturn(employees);
+
+        int expected = 0;
+        int actual = testObject.payEmployees();
+        Assertions.assertEquals(expected,actual);
+    }
+
+    @Test
+    void payEmployeesMockitoExceptionTest(){
+        EmployeeRepository mockRepo = mock(EmployeeRepository.class);
+        BankService mockBank = mock(BankService.class);
+        EmployeeManager testObject = new EmployeeManager(mockRepo,mockBank);
+
+        ArrayList<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(ID, SALARY));
+        when(mockRepo.findAll()).thenReturn(employees);
+        doThrow(new RuntimeException()).when(mockBank).pay(ID, SALARY);
+
+        int expectedPayments = 0;
+        boolean expectedPaid = false;
+        int actual = testObject.payEmployees();
+
+        Assertions.assertEquals(expectedPayments, actual);
+        Assertions.assertEquals(expectedPaid, employees.get(0).isPaid());
     }
 
 }
